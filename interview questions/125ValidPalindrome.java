53b70184297c153e15837349b4d5196fe36a2ac7
hic <minping.yang@yahoo.com>
2017-05-24 23:42:33 +1200

    first commit

---
+++ interview questions/125ValidPalindrome.java
@@ -0,0 +1,28 @@
+
+// Given a string, determine if it is a palindrome, 
+//considering only alphanumeric characters and ignoring cases.
+
+// For example,
+// "A man, a plan, a canal: Panama" is a palindrome.
+// "race a car" is not a palindrome.
+
+// Note:
+// Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+// For the purpose of this problem, we define empty string as valid palindrome.
+
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        // if s is null
+        if(s == null)
+            return true;
+        //else
+        String strings[] = s.split(",");
+        for(String s: strings){
+            String innerStrings[] = strings.split(" ");
+            innerStrings[0].equalsIgnoreCase("a");
+        }
+        
+    }
+}
